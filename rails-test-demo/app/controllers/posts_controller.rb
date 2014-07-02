15fce1fd067ce8e16d583acaafdaa8018c3df67e
Alessandro Stein <alessandro@alessandrostein.com>
2014-07-02 20:57:14 -0300

    post

--- rails-test-demo/app/controllers/posts_controller.rb
+++ rails-test-demo/app/controllers/posts_controller.rb
@@ -5,7 +5,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    render text: params[:post].inspect
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
 
 end
