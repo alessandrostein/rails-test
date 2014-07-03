a152cbfd54f548c4d8b1c09fd646b9d2f246021a
Alessandro Stein <alessandro@alessandrostein.com>
2014-07-02 21:38:55 -0300

    Controller

--- rails-test-demo/app/controllers/posts_controller.rb
+++ rails-test-demo/app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
 
   def new
+    @post = Post.new
+  end
 
+  def index
+    @posts = Post.all
   end
 
   def create
@@ -11,17 +15,14 @@ class PostsController < ApplicationController
     redirect_to @post
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :text)
     end
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
-  def index
-    @posts = Post.all
-  end
 
 end
