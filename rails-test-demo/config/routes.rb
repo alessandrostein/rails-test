e07607fd2800576c0314858fdcdb42ef08f94ea8
Alessandro Stein <alessandro@alessandrostein.com>
2014-07-03 00:39:29 -0300

    index

--- rails-test-demo/config/routes.rb
+++ rails-test-demo/config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   root 'we#lcome#index'#
 
-  posts GET    /posts/:id(.:format)      posts#show
-  posts GET    /posts(.:format)          posts#index
+  #posts GET    /posts/:id(.:format)      posts#show
+  #posts GET    /posts(.:format)          posts#index
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
